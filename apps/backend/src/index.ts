ba8dc9519c51a1cda1694abee210e8525c198887
KingRainbow44 <kobedo11@gmail.com>
2025-05-10 13:06:27 -0400

    feat(backend): add test for Elysia app

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,14 @@
 import Elysia from "elysia";
 
 const app = new Elysia()
-    .get("/health", () => "OK");
+    .get("/health", () => "OK")
+    .listen(3000, listenCallback);
+
+function listenCallback(): void {
+    console.log("🔥 Web server is running on http://localhost:3000");
+}
+
+export default app;
 
 /** This export is used with '@elysiajs/eden'. */
 export type App = typeof app;
---
+++ apps/backend/test/index.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from "bun:test";
+
+import app from "@/index.ts";
+
+describe("Elysia", () => {
+    it("should return a response", async () => {
+        const response = await app
+            .handle(new Request("http://localhost:3000/health"))
+            .then((res) => res.text());
+
+        expect(response).toBe("OK");
+    });
+});
