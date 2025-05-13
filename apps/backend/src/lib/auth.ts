336549f3548de887ad33c2cdfc87b111446ffcf4
KingRainbow44 <kobedo11@gmail.com>
2025-05-13 19:42:44 -0400

    fix(backend): fallback URI if environment variable is not defined

--- apps/backend/src/lib/auth.ts
+++ apps/backend/src/lib/auth.ts
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.MONGODB_URI);
+const client = new MongoClient(process.env.MONGODB_URI ?? "mongodb://localhost:27017");
 const database = client.db();
 
 export const auth = betterAuth({
