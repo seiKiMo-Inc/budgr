fdbe47522fee058d762ac1677706640f6f1090d7
KingRainbow44 <kobedo11@gmail.com>
2025-06-03 19:19:41 -0400

    misc(mobile): add iOS development build/debug

--- apps/mobile/src/env.d.ts
+++ apps/mobile/src/env.d.ts
@@ -1,8 +1,6 @@
-declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            EXPO_PUBLIC_BASE_URL: string;
-            EXPO_PUBLIC_AUTH_PATH: string;
-        }
+declare module "NodeJS" {
+    interface ProcessEnv {
+        EXPO_PUBLIC_BASE_URL: string;
+        EXPO_PUBLIC_AUTH_PATH: string;
     }
 }
